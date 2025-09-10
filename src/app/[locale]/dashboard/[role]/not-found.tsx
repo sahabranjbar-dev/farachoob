2929d31a0129b4a0dc0e6c4212c1776160a9e8c0
sahab <sahab@sahabs-MacBook-Air.local>
2025-09-10 21:45:55 +0330

    modify not-found content and set it server side

--- src/app/[locale]/dashboard/[role]/not-found.tsx
+++ src/app/[locale]/dashboard/[role]/not-found.tsx
@@ -1,25 +1,9 @@
-"use client";
-
-import { Button } from "@/components/ui/button";
-import { useRouter } from "next/navigation";
-import { Player } from "@lordicon/react";
-import error404 from "../../../../assets/icons/wired-flat-1140-error-hover-oscillate.json";
-import { motion } from "framer-motion";
+import { Link } from "@/i18n/navigation";
+import { routing } from "@/i18n/routing";
 
 export default function NotFoundPage() {
-  const router = useRouter();
-
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900 text-center px-4">
-      <motion.div
-        initial={{ scale: 0.8, opacity: 0 }}
-        animate={{ scale: 1, opacity: 1 }}
-        transition={{ type: "spring", stiffness: 150, damping: 10 }}
-        className="w-64 h-64"
-      >
-        <Player size={100} icon={error404} />
-      </motion.div>
-
       <h1 className="text-4xl font-bold mt-6 text-gray-800 dark:text-white">
         صفحه پیدا نشد!
       </h1>
@@ -29,13 +13,9 @@ export default function NotFoundPage() {
         باشد.
       </p>
 
-      <Button
-        className="mt-6"
-        onClick={() => router.push("/")}
-        variant="default"
-      >
+      <Link className="mt-6" href={`/${routing.defaultLocale}/dashboard`}>
         بازگشت به صفحه اصلی
-      </Button>
+      </Link>
     </div>
   );
 }
